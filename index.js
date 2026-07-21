7cc4a0bc7e7b695f6c959129bbc09e617b21ed2d
m0nusharma <sharmamonu42969@gmail.com>
2026-07-21 15:34:08 +0530

    dsa

---
+++ index.js
@@ -0,0 +1,22 @@
+// can vote
+function isVote(age){
+  if (age >= 18){
+    console.log("YES , you can vOte")
+  }else{
+    console.log("NO , you can't vote")
+  }
+  return age 
+} 
+ isVote(55)
+
+//  odd even number 
+function isEven(num){
+    if(num % 2 === 0){
+        console.log("its even number")
+    }else{
+        console.log("its a odd number")
+    }
+    return num
+}
+
+isEven(11)
